2ac6aec871720e7a2d283982c1f70392386d52cd
Reach <semreach.info@gmail.com>
2024-09-27 15:35:15 +0700

    add text on form login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,4 @@
 <template>
     <h2>Form Login</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, sed ut modi consectetur laborum similique quibusdam cum ea deleniti a asperiores iure aspernatur blanditiis tempora natus alias deserunt consequuntur! Non!</p>
 </template>
